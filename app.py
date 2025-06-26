8814c75569251d6b19e15053f62f158de52cd013
Dennis Wachira <wachiradenis336@gmail.com>
2025-06-26 21:47:59 +0300

    feat: add flask migrate functionality for db migrations

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from extensions import db
+from extensions import db, migrate
 from models.episode import Episode
 from models.guest import Guest
 from models.appearance import Appearance
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+migrate.init_app(app, db)
 
 # Optional: if you want db.create_all() on launch
 # with app.app_context():
